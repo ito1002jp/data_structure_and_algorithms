451852bbf77151893f755b1dff20f5c918be668f
Ohkura Tessyuu <ito1002jp@gmail.com>
2022-12-03 18:56:12 +0900

    1.2 ver2

--- Chapter1/1.2.java
+++ Chapter1/1.2.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        boolean result = checkPermutation("permission", "missionper");
+        boolean result = checkPermutation3("permission", "missionper");
         System.out.println(result);
     }
     
@@ -14,6 +14,7 @@ public class Main {
     
     /**
      * ソートして文字列を比較する
+     * O(NLogN)
      **/
     public static boolean checkPermutation(String word_a, String word_b) {
         if (word_a.length() != word_b.length()) return false;
@@ -37,6 +38,27 @@ public class Main {
         
         // 文字をカウントした配列が同じ場合は二つの文字列は順列である。
         return Arrays.equals(char_set_a, char_set_b);
+    }
+    
+     /**
+     * 文字を数えて比較する
+     * O(N)
+     **/
+    public static boolean checkPermutation3(String word_a, String word_b) {
+        if (word_a.length() != word_b.length()) return false;
+        
+        // basic ASCIIの文字数だけ配列を用意する
+        int[] char_set = new int[128];
+        for (int i = 0; i < word_a.length(); i++) {
+            char_set[(int)word_a.charAt(i)]++;
+        }
+        for (int i = 0; i < word_b.length(); i++) {
+            char_set[(int)word_b.charAt(i)]--;
+            if (char_set[(int)word_b.charAt(i)] < 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
 
