ea099721ed06bb2cbf6d1c82dfaaffaa351bab4e
Ohkura Tessyuu <ito1002jp@gmail.com>
2022-12-06 23:39:54 +0900

    1.4 ver2

--- Chapter1/1.4.java
+++ Chapter1/1.4.java
@@ -6,11 +6,41 @@ public class Main {
         // System.out.println(checkPalindrome("Tact Coaa")); // false
     }
     
+    // public static boolean checkPalindrome(String word) {
+    //     word = word.toLowerCase();
+    //     // アルファベット小文字の数だけ配列を用意する。
+    //     int[] char_set = new int[Character.getNumericValue('z') - Character.getNumericValue('a') + 1];
+    //     int val = 0;
+    //     for (int i = 0; i < word.length(); i++) {
+    //         val = getCharNum(word.charAt(i));
+    //         // a-z以外はスキップ
+    //         if (val < 0) {
+    //             continue;
+    //         }
+    //         char_set[val]++;
+    //     }
+        
+    //     // 回文かどうかチェックする
+    //     // 奇数のアルファベットが複数存在しないことを確認
+    //     boolean foundOdd = false;
+    //     for (int i = 0; i < char_set.length; i++) {
+    //         if (char_set[i] %2 == 1) {
+    //             if (foundOdd) {
+    //                 return false;
+    //             }
+    //             foundOdd = true;
+    //         }
+    //     }
+        
+    //     return true;
+    // }
+    
     public static boolean checkPalindrome(String word) {
         word = word.toLowerCase();
         // アルファベット小文字の数だけ配列を用意する。
         int[] char_set = new int[Character.getNumericValue('z') - Character.getNumericValue('a') + 1];
         int val = 0;
+        int countOdd = 0;
         for (int i = 0; i < word.length(); i++) {
             val = getCharNum(word.charAt(i));
             // a-z以外はスキップ
@@ -18,21 +48,17 @@ public class Main {
                 continue;
             }
             char_set[val]++;
-        }
-        
-        // 回文かどうかチェックする
-        // 奇数のアルファベットが複数存在しないことを確認
-        boolean foundOdd = false;
-        for (int i = 0; i < char_set.length; i++) {
-            if (char_set[i] %2 == 1) {
-                if (foundOdd) {
-                    return false;
-                }
-                foundOdd = true;
+            
+            // アルファベットの個数が奇数のものをカウントする
+            if (char_set[val] % 2 == 1) {
+                countOdd++;
+            } else {
+                countOdd--;
             }
         }
         
-        return true;
+        // 奇数のアルファベットが0、１個以外の場合、回文ではない
+        return countOdd <= 1;
     }
     
     public static int getCharNum(char c) {
