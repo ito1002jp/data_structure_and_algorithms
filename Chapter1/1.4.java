952137e3a355d1467a318b63c518abd28331c354
Ohkura Tessyuu <ito1002jp@gmail.com>
2022-12-06 23:28:46 +0900

    1.4

---
+++ Chapter1/1.4.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.println(checkPalindrome("Tact Coa")); // true
+        // System.out.println(checkPalindrome("Tact Coaa")); // false
+    }
+    
+    public static boolean checkPalindrome(String word) {
+        word = word.toLowerCase();
+        // アルファベット小文字の数だけ配列を用意する。
+        int[] char_set = new int[Character.getNumericValue('z') - Character.getNumericValue('a') + 1];
+        int val = 0;
+        for (int i = 0; i < word.length(); i++) {
+            val = getCharNum(word.charAt(i));
+            // a-z以外はスキップ
+            if (val < 0) {
+                continue;
+            }
+            char_set[val]++;
+        }
+        
+        // 回文かどうかチェックする
+        // 奇数のアルファベットが複数存在しないことを確認
+        boolean foundOdd = false;
+        for (int i = 0; i < char_set.length; i++) {
+            if (char_set[i] %2 == 1) {
+                if (foundOdd) {
+                    return false;
+                }
+                foundOdd = true;
+            }
+        }
+        
+        return true;
+    }
+    
+    public static int getCharNum(char c) {
+        int a = Character.getNumericValue('a');
+        int z = Character.getNumericValue('z');
+        int val = Character.getNumericValue(c);
+        
+        // アルファベット以外は全て-1で返す
+        if (val < a && z < val) {
+            return -1;
+        }
+        
+        return val - a;
+    }
+}
+
