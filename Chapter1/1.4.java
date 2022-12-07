4c2808ae37856a0e919e34201e36ab7ea2f2ac9f
Ohkura Tessyuu <ito1002jp@gmail.com>
2022-12-07 11:28:23 +0900

    1.4 ver3

--- Chapter1/1.4.java
+++ Chapter1/1.4.java
@@ -2,8 +2,8 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println(checkPalindrome("Tact Coa")); // true
-        // System.out.println(checkPalindrome("Tact Coaa")); // false
+        // System.out.println(checkPalindrome("Tact Coa")); // true
+        System.out.println(checkPalindrome("Tact Coaa")); // false
     }
     
     // public static boolean checkPalindrome(String word) {
@@ -35,30 +35,46 @@ public class Main {
     //     return true;
     // }
     
+    // public static boolean checkPalindrome(String word) {
+    //     word = word.toLowerCase();
+    //     // アルファベット小文字の数だけ配列を用意する。
+    //     int[] char_set = new int[Character.getNumericValue('z') - Character.getNumericValue('a') + 1];
+    //     int val = 0;
+    //     int countOdd = 0;
+    //     for (int i = 0; i < word.length(); i++) {
+    //         val = getCharNum(word.charAt(i));
+    //         // a-z以外はスキップ
+    //         if (val < 0) {
+    //             continue;
+    //         }
+    //         char_set[val]++;
+            
+    //         // アルファベットの個数が奇数のものをカウントする
+    //         if (char_set[val] % 2 == 1) {
+    //             countOdd++;
+    //         } else {
+    //             countOdd--;
+    //         }
+    //     }
+        
+    //     // 奇数のアルファベットが0、１個以外の場合、回文ではない
+    //     return countOdd <= 1;
+    // }
+    
     public static boolean checkPalindrome(String word) {
-        word = word.toLowerCase();
-        // アルファベット小文字の数だけ配列を用意する。
-        int[] char_set = new int[Character.getNumericValue('z') - Character.getNumericValue('a') + 1];
+        int bitVector = 0;
         int val = 0;
-        int countOdd = 0;
         for (int i = 0; i < word.length(); i++) {
             val = getCharNum(word.charAt(i));
             // a-z以外はスキップ
             if (val < 0) {
                 continue;
             }
-            char_set[val]++;
-            
-            // アルファベットの個数が奇数のものをカウントする
-            if (char_set[val] % 2 == 1) {
-                countOdd++;
-            } else {
-                countOdd--;
-            }
+            bitVector = toggle(bitVector, val);
         }
-        
-        // 奇数のアルファベットが0、１個以外の場合、回文ではない
-        return countOdd <= 1;
+    
+        System.out.println(Integer.toBinaryString(bitVector));
+        return (bitVector & (bitVector - 1)) == 0;
     }
     
     public static int getCharNum(char c) {
@@ -72,6 +88,20 @@ public class Main {
         }
         
         return val - a;
+    }
+
+    public static int toggle(int bitVector, int index) {
+        int mask = 1 << index;
+        
+        // 奇数=>偶数に切り替える必要がない場合
+        if ((bitVector & mask) == 0) {
+            bitVector |= mask;
+        } else {
+            // 奇数=>偶数に切り替える場合
+            bitVector &= ~mask;
+        }
+        
+        return bitVector;
     }
 }
 
